b9f041b6c9c910ebfbb4f47f98a9bd1da7cf89fd
sterdayye <sterdayye@gmail.com>
2017-09-19 17:22:30 +0300

    Update desu1.sh

--- desu1.sh
+++ desu1.sh
@@ -33,10 +33,10 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/ying17zi/vim-live-latex-preview.git
 mv vim-live-latex-preview ~/.vim/bundle/ 
 
-curl https://github.com/sterdayye/voidrice/blob/master/desu1.sh > /home/$USER/desu1.sh
+curl https://github.com/sterdayye/voidrice/blob/master/desu2.sh > /home/$USER/desu2.sh
 
 echo "root system configuration complete."
 echo ""
 echo "Now log out of root (ctrl-d), log in as $USER and run the following command:"
 echo ""
-echo "bash desu1.sh"
+echo "bash desu2.sh"
